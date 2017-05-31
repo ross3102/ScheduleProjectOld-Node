f347d1530db8241bb0b42043abb150a370b235f2
Ross3102 <rosnew20@bergen.org>
2017-05-31 07:55:58 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express   = require('express'),
+      app       = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+   res.send('<h1>Homepage!</h1>'); 
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
